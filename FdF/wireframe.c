c11d050028ba6fdade731595938e79a6b7023061
Edugonza <edugonza@student.42berlin.de>
2025-03-19 17:57:49 +0100

    FdF first approach

--- FdF/wireframe.c
+++ FdF/wireframe.c
@@ -6,21 +6,44 @@
 /*   By: edugonza <edugonza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:50:14 by edugonza          #+#    #+#             */
-/*   Updated: 2025/03/17 15:37:31 by edugonza         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:58:52 by edugonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wireframe.h"
 
+int	handle_input(int keysym, t_vars *mlx_data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(mlx_data->ptr, mlx_data->win);
+		mlx_destroy_display(mlx_data->ptr);
+		free(mlx_data->ptr);
+		exit(1);
+	}
+	ft_printf("You pressed %d\n", keysym);
+	return(1);	
+}
 int	main(void)
 {
-	void *mlx_ptr;
+	t_vars mlx_data;
 
-	mlx_ptr = mlx_init();
-	if (mlx_ptr == NULL)
+	mlx_data.ptr = mlx_init();
+	if (mlx_data.ptr == NULL)
 		return (0);
 
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+	mlx_data.win = mlx_new_window(mlx_data.ptr, 500, 400, "Main");
+	if (mlx_data.win == NULL)
+	{
+		mlx_destroy_display(mlx_data.ptr);
+		free(mlx_data.ptr);
+		return (0);
+	}
+
+	mlx_key_hook(mlx_data.win, handle_input, &mlx_data);
+	mlx_loop(mlx_data.ptr);
+	mlx_destroy_window(mlx_data.ptr, mlx_data.win);
+	mlx_destroy_display(mlx_data.ptr);
+	free(mlx_data.ptr);
 	return(1);
 }
--- FdF/wireframe.h
+++ FdF/wireframe.h
@@ -6,7 +6,7 @@
 /*   By: edugonza <edugonza@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:50:31 by edugonza          #+#    #+#             */
-/*   Updated: 2025/03/17 15:35:01 by edugonza         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:57:17 by edugonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 
 # include <stdlib.h>
 # include "minilibx-linux/mlx.h"
+# include <X11/keysym.h>
 // # include <mlx.h>
 # include "Libft_power/ft_printf/ft_printf.h"
 # include "libft.h"
+
+typedef struct	s_vars {
+	void	*ptr;
+	void	*win;
+}				t_vars;
 
 //Main
 int	main(void);
