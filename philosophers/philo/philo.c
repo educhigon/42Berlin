a6e69587e03c6796f6ebb5c22ffdbf68562ea899
Edugonza <edugonza@student.42berlin.de>
2025-07-23 16:38:55 +0200

    norminette done

--- philosophers/philo/philo.c
+++ philosophers/philo/philo.c
@@ -59,64 +59,52 @@ void	*thread_func(void *arg)
 		philo_release_fork(phi, phi->table, 0);
 		return (NULL);
 	}
-	// usleep(phi->num_philo);
-	// printf("Thread %d: Starting\n", phi->num_philo);
 	philo_seq(phi);
-	// printf("Thread %d: Ending\n", phi->num_philo);
 	return (NULL);
 }
 
-void	create_philos(t_philo *phi, int i, t_data *table)
+void	loop_philos(t_data *tb, int havent_eaten_enough, struct timeval now)
 {
-	phi->num_philo = i + 1;
-	phi->time_last_eaten = table->dinner_start;
-	phi->times_eaten = 0;
-	pthread_mutex_init(&phi->time_last_eaten_mutex, NULL);
-	pthread_mutex_init(&phi->times_eaten_mutex, NULL);
-	phi->table = table;
-	pthread_create(&phi->philo_thread, NULL, thread_func, phi);
-	return ;
+	int	i;
+
+	i = -1;
+	while (++i < tb->num_philos && !tb->philo_dead)
+	{
+		pthread_mutex_lock(&tb->philos[i].times_eaten_mutex);
+		pthread_mutex_lock(&tb->philos[i].time_last_eaten_mutex);
+		if (tb->philos[i].times_eaten < tb->num_must_eat)
+			havent_eaten_enough = 0;
+		if (time_math(tb->philos[i].time_last_eaten, now) > 
+			tb->tt_die && !havent_eaten_enough)
+		{
+			print_status(tb->philos[i].num_philo, tb, "died");
+			tb->philo_dead = 1;
+		}
+		pthread_mutex_unlock(&tb->philos[i].time_last_eaten_mutex);
+		pthread_mutex_unlock(&tb->philos[i].times_eaten_mutex);
+	}
 }
 
 void	*global_monitor(void *arg)
 {
 	struct timeval	now;
 	t_data			*tb;
-	int				i;
 	int				havent_eaten_enough;
 
 	tb = (t_data *)arg;
-
 	while (1)
 	{
 		havent_eaten_enough = 1;
-		if(tb->num_must_eat == -1)
+		if (tb->num_must_eat == -1)
 			havent_eaten_enough = 0;
 		usleep(1);
-
 		pthread_mutex_lock(&tb->philo_dead_mutex);
 		gettimeofday(&now, NULL);
-
-		i = -1;
-		while (++i < tb->num_philos && !tb->philo_dead)
-		{
-			pthread_mutex_lock(&tb->philos[i].times_eaten_mutex);
-			pthread_mutex_lock(&tb->philos[i].time_last_eaten_mutex);
-
-			if (tb->philos[i].times_eaten < tb->num_must_eat)
-				havent_eaten_enough = 0;
-			if (time_math(tb->philos[i].time_last_eaten, now) > tb->tt_die && !havent_eaten_enough)
-			{
-				print_status(tb->philos[i].num_philo, tb, "died");
-				tb->philo_dead = 1;
-			}
-			pthread_mutex_unlock(&tb->philos[i].time_last_eaten_mutex);
-			pthread_mutex_unlock(&tb->philos[i].times_eaten_mutex);
-		}
-		if(tb->philo_dead || havent_eaten_enough)
+		loop_philos(tb, havent_eaten_enough, now);
+		if (tb->philo_dead || havent_eaten_enough)
 		{
 			pthread_mutex_unlock(&tb->philo_dead_mutex);
-			break;
+			break ;
 		}
 		pthread_mutex_unlock(&tb->philo_dead_mutex);
 	}
@@ -139,7 +127,7 @@ int	main(int ac, char **av)
 		pthread_mutex_init(&table.forks[i], NULL);
 		create_philos(&table.philos[i], i, &table);
 		precise_sleep(table.dinner_start,
-			5 , &table.philos[i]);
+			5, &table.philos[i]);
 	}
 	pthread_create(&monitor, NULL, global_monitor, &table);
 	i = -1;
