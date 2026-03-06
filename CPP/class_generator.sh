cf226553a50325b4f57e9d07d7f240efc3c4613f
EDUARDO GONZALO <educhigon@gmail.com>
2026-03-06 20:18:37 +0100

    adding color glossary

--- CPP/class_generator.sh
+++ CPP/class_generator.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Black	\033[30m
+# Red	\033[31m
+# Green	\033[32m
+# Yellow	\033[33m
+# Blue	\033[34m
+# Magenta	\033[35m
+# Cyan	\033[36m
+# White	\033[37m
+# Reset	\033[0m
+
+# Black	\033[40m
+# Red	\033[41m
+# Green	\033[42m
+# Yellow	\033[43m
+# Blue	\033[44m
+# Magenta	\033[45m
+# Cyan	\033[46m
+# White	\033[47m
+
+
+
 # Check if an argument is provided
 if [ $# -lt 1 ]; then
   echo "Usage: $0 <name_with_underscores> *<destination_folder>"
