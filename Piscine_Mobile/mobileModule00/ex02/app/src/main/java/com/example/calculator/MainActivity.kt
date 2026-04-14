c107e1128f1981a3470a5ab62772d7e361cd9b4a
Edugonza <edugonza@student.42berlin.de>
2026-04-14 13:46:25 +0200

    Calculator shell - ex02 done

--- Piscine_Mobile/mobileModule00/ex02/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Piscine_Mobile/mobileModule00/ex02/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,14 +2,21 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.i
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Grid
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -20,10 +27,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
@@ -45,16 +55,22 @@ class MainActivity : ComponentActivity() {
                     topBar = { TopAppBar() }  // ← belongs here
                 ) { innerPadding ->
                     Column (
-                        modifier = Modifier.fillMaxHeight().padding(16.dp),
-                        verticalArrangement = Arrangement.Bottom
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.SpaceBetween
                     ) {
                         ResultFields(
-                            name = "text",
-                            modifier = Modifier.fillMaxSize().padding(innerPadding),
+                            modifier = Modifier
+//                                .fillMaxSize()
+                                .fillMaxWidth()
+                                .weight(4f)
                         )
                         Keypad(
-                            name = "text",
-                            modifier = Modifier.padding(innerPadding)
+                            modifier = Modifier
+//                                .fillMaxSize()
+                                .fillMaxWidth()
+                                .weight(6f)
                         )
                     }
                 }
@@ -83,58 +99,71 @@ fun TopAppBar() {
 }
 
 @Composable
-fun ResultFields(name: String, modifier: Modifier = Modifier) {
+fun ResultFields(modifier: Modifier = Modifier) {
 
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "0x0",
+//            maxLines = 10,
+            fontSize = 50.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier.align(Alignment.End)
+                .fillMaxWidth()
+        )
 
-    Text(
-        buildAnnotatedString {
-            withStyle(style = ParagraphStyle(lineHeight = 100.sp)) {
-                append("0x0")
-            }
-        },
-        maxLines = 10,
-        fontSize = 50.sp,
-        overflow = TextOverflow.Ellipsis,
-        textAlign = TextAlign.Right,
-        modifier = Modifier.width(800.dp)
-    )
+        Text(
+            text = "00000",
+//            maxLines = 10,
+            fontSize = 50.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier.align(Alignment.End)
+                .fillMaxWidth()
+        )
+    }
 
-    Text(
-        buildAnnotatedString {
-            withStyle(style = ParagraphStyle(lineHeight = 100.sp)) {
-                append("00000")
-            }
-        },
-        maxLines = 10,
-        fontSize = 50.sp,
-        overflow = TextOverflow.Ellipsis,
-        textAlign = TextAlign.Right,
-        modifier = Modifier.width(800.dp)
-    )
 }
 @Composable
-fun Keypad(name: String, modifier: Modifier = Modifier) {
+fun Keypad(modifier: Modifier = Modifier) {
 
-    val buttons = listOf(
-        "7", "8", "9", "C", "AC",
-        "4", "5", "6", "+", "-",
-        "1", "2", "3", "*", "/",
-        "0", ".", "00", "=", ""
-    )
+    val buttons1 = listOf("7", "8", "9", "C", "AC")
+    val buttons2 = listOf("4", "5", "6", "+", "-")
+    val buttons3 = listOf("1", "2", "3", "*", "/")
+    val buttons4 = listOf("0", ".", "00", "=", "")
+    val buttons = listOf(buttons1, buttons2, buttons3, buttons4)
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(5)  // 5 columns
+    Column(
+        modifier = modifier
+            .fillMaxSize()
     ) {
-        items(buttons.size)   { i ->
-            Button(onClick = {
-//                println("Button "); println(buttons.elementAt(i)); println(" clicked")
-                Log.d("TAG", "Button ${buttons[i]} clicked")   // prints to Android Logcat
-            }) {
-                Text(
-                    text = buttons[i],
-                    fontSize = 20.sp,
-                    modifier = modifier
-                )
+        buttons.forEach {
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                it.forEach { label ->
+                    val textColor = when (label) {
+                        "AC", "C" -> Color.Red
+                        "+", "-", "*", "/" -> Color.White
+                        else -> Color.Gray
+                    }
+                    TextButton(onClick = {Log.d("TAG", "Button $label clicked")},
+                        modifier = modifier
+                            .fillMaxSize()
+                            .weight(1f)
+                    ) {
+                        Text(
+                            text = label,
+                            fontSize = 30.sp,
+                            color = textColor
+                        )
+                    }
+                }
             }
         }
     }
@@ -153,11 +182,9 @@ fun GreetingPreview() {
                 verticalArrangement = Arrangement.Bottom
             ) {
                 ResultFields(
-                    name = "text",
-                    modifier = Modifier.fillMaxSize().padding(innerPadding),
+                    modifier = Modifier.fillMaxSize().padding(innerPadding)
                 )
                 Keypad(
-                    name = "text",
                     modifier = Modifier.padding(innerPadding)
                 )
             }
